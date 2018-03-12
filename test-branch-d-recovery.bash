db14c85215b6d4c36efa0d2483689497439ad44a
Kuniwak <orga.chem.job@gmail.com>
2018-03-12 16:52:35 +0900

    Add tests for git branch -d/-D

---
+++ test-branch-d-recovery.bash
@@ -0,0 +1,36 @@
+#!/bin/bash -eux
+set -o pipefail
+
+
+WORKSPACE=$(mktemp -d ./reset-branch-d-recovery.XXXXXX)
+
+(cd $WORKSPACE
+  git init
+
+  echo a > a
+  echo b > b
+  echo c > c
+  echo d > d
+
+  git add a
+  git commit -m "Add a"
+
+  git checkout -b branch-b
+
+  git add c
+  git commit -m "Add c"
+
+  git add d
+  git commit -m "Add d"
+
+  git checkout master
+
+  git add b
+  git commit -m "Add b"
+
+  git branch -D branch-b
+
+  git reflog
+)
+
+rmtrash $WORKSPACE
