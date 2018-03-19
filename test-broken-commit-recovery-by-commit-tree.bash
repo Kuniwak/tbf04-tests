be0b22772aafdad97fd02c584f86e726db598560
Kuniwak <orga.chem.job@gmail.com>
2018-03-20 02:11:01 +0900

    Add more tests

---
+++ test-broken-commit-recovery-by-commit-tree.bash
@@ -0,0 +1,71 @@
+#!/bin/bash -eux
+set -o pipefail
+
+
+WORKSPACE=$(mktemp -d ./broken-commit.XXXXXX)
+
+(cd $WORKSPACE
+  mkdir remote
+
+  (cd remote
+    git init
+
+    echo a > a
+    echo c > c
+    echo d > d
+
+    git add a
+    git commit -m "Add a"
+
+    git checkout -b branch-b
+
+    git add c
+    git commit -m "Add c"
+
+    git add d
+    git commit -m "Add d"
+
+    git checkout master
+  )
+
+  git clone ./remote ./broken --no-local
+  (cd broken
+    PACKFILE=$(find .git/objects/pack -name 'pack-*.pack')
+    echo b > b
+    mkdir e
+    echo e > e/e
+
+    git add b
+    git commit -m "Add b"
+
+    git add e/e
+    git commit -m "Add e"
+
+    COMMIT=$(git rev-parse HEAD^^)
+    COMMIT_FILE=$(echo $COMMIT | sed -e 's/\(..\)\(.*\)/.git\/objects\/\1\/\2/')
+
+    git cat-file -p $COMMIT
+
+    git fsck
+
+    mv $PACKFILE ./pack
+    git unpack-objects < ./pack
+    rmtrash ./pack
+
+    chmod +w $COMMIT_FILE
+    echo > $COMMIT_FILE
+    chmod -x $COMMIT_FILE
+
+		set +e
+    git fsck
+		echo $?
+		set -e
+  )
+
+  (cd ./broken
+    git gc
+    git fsck
+  )
+)
+
+rmtrash $WORKSPACE
---
+++ test-broken-pack-recovery-by-pack-grafting.bash
@@ -0,0 +1,62 @@
+#!/bin/bash -eux
+set -o pipefail
+
+
+WORKSPACE=$(mktemp -d ./broken-commit.XXXXXX)
+
+(cd $WORKSPACE
+  mkdir remote
+  export GIT_AUTHOR_DATE='1521450633 +0900'
+  export GIT_COMMITTER_DATE='1521450633 +0900'
+
+  (cd remote
+    git init
+    echo a > a
+    echo b > b
+    echo c > c
+
+    git add a
+    git commit -m "Add a"
+
+    git add b
+    git commit -m "Add b"
+
+    git add c
+    git commit -m "Add c"
+  )
+
+  git clone ./remote ./broken --no-local
+  (cd broken
+    PACKFILE=$(find .git/objects/pack -name 'pack-*.pack')
+    echo d > d
+
+    git add d
+    git commit -m "Add d"
+
+    COMMIT=$(git rev-parse HEAD^^)
+
+    git cat-file -p $COMMIT
+
+    git fsck
+
+    mv $PACKFILE ./pack
+    git unpack-objects < ./pack
+    rmtrash ./pack
+
+    COMMIT_FILE=$(echo $COMMIT | sed -e 's/\(..\)\(.*\)/.git\/objects\/\1\/\2/')
+    rm  $COMMIT_FILE
+
+    git fsck || true
+  )
+
+  git clone ./remote ./re-cloned --no-local
+  ALTER_PACKFILE=$(find re-cloned/.git/objects/pack -name 'pack-*.pack')
+  PACKFILE_BASENAME=$(basename $ALTER_PACKFILE)
+  mv $ALTER_PACKFILE ./broken/.git/objects/pack/$PACKFILE_BASENAME
+
+  (cd ./broken
+    git fsck && echo OK || echo NG
+  )
+)
+
+rmtrash $WORKSPACE
