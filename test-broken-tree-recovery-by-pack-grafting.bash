59f2920f02a17e60ef552fc20756b8265eaa918b
Kuniwak <orga.chem.job@gmail.com>
2018-03-20 17:38:52 +0900

    Add broken-tree recovery tests

---
+++ test-broken-tree-recovery-by-pack-grafting.bash
@@ -0,0 +1,69 @@
+#!/bin/bash -eux
+set -o pipefail
+
+
+WORKSPACE=$(mktemp -d ./broken-commit.XXXXXX)
+
+(cd $WORKSPACE
+  mkdir remote
+
+  (cd remote
+    git init
+    echo a > a
+    echo b > b
+    echo c > c
+
+    export GIT_AUTHOR_DATE='1521450633 +0900'
+    export GIT_COMMITTER_DATE='1521450633 +0900'
+    git add a
+    git commit -m "Add a"
+
+    export GIT_AUTHOR_DATE='1521450693 +0900'
+    export GIT_COMMITTER_DATE='1521450693 +0900'
+    git add b
+    git commit -m "Add b"
+
+    export GIT_AUTHOR_DATE='1521450753 +0900'
+    export GIT_COMMITTER_DATE='1521450753 +0900'
+    git add c
+    git commit -m "Add c"
+  )
+
+  git clone ./remote ./broken --no-local
+  (cd broken
+    PACKFILE=$(find .git/objects/pack -name 'pack-*.pack')
+    echo d > d
+
+    export GIT_AUTHOR_DATE='1521450753 +0900'
+    export GIT_COMMITTER_DATE='1521450753 +0900'
+    git add d
+    git commit -m "Add d"
+
+    COMMIT=$(git rev-parse HEAD^^)
+
+    git cat-file -p $COMMIT
+
+    git fsck
+
+    mv $PACKFILE ./pack
+    git unpack-objects < ./pack
+    rmtrash ./pack
+
+    TREE=$(git cat-file -p HEAD^^ | head -1 | sed -e 's/^tree //')
+    TREE_FILE=$(echo $TREE | sed -e 's/\(..\)\(.*\)/.git\/objects\/\1\/\2/')
+    rm  $TREE_FILE
+
+    git fsck || true
+  )
+
+  git clone ./remote ./re-cloned --no-local
+  ALTER_PACKFILE=$(find re-cloned/.git/objects/pack -name 'pack-*.pack')
+  PACKFILE_BASENAME=$(basename $ALTER_PACKFILE)
+  mv $ALTER_PACKFILE ./broken/.git/objects/pack/$PACKFILE_BASENAME
+
+  (cd ./broken
+    git fsck && echo OK || echo NG
+  )
+)
+
+rmtrash $WORKSPACE
