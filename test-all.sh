2da98ee86d28804989eca2d7aaca888df2f3bd65
Kuniwak <orga.chem.job@gmail.com>
2018-03-27 14:13:56 +0900

    Try to fix on CI

--- test-all.sh
+++ test-all.sh
@@ -1,7 +1,16 @@
 #!/bin/sh -eu
 
 base_dir=$(cd $(dirname $0); pwd)
+failed=0
 
 for test in $(find ./tests -type f); do
-  $test && echo -e "OK: $test\n\n" || echo -e "NG: $test\n\n"
+	echo "BEGIN $test"
+  if $test; then
+    echo "OK: $test"
+  else
+    echo "NG: $test"
+    failed=1
+  fi
 done
+
+exit $failed
